71c99440a74c340ecc848e79eb16743c893b903b
Andrew Menich <andrew@simplygoodwork.com>
2022-10-19 14:07:44 -0400

    update header styles

--- craft/config/db.php
+++ craft/config/db.php
@@ -6,7 +6,6 @@
  * All of your system's database configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
-
 return array(
     'server' => getenv('DB_HOST'),
     'user' => getenv('DB_USER'),
